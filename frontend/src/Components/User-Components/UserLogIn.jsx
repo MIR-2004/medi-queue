8b6fff909d710da3b30d5d8f3df289bc7788b331
Mir Saif Ali <mir.saif.ali2004@gmail.com>
2024-09-14 09:24:56 -0700

    no

--- frontend/src/Components/User-Components/UserLogIn.jsx
+++ frontend/src/Components/User-Components/UserLogIn.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const UserLogIn = () => {
   return (
-    <div>UserLogIn</div>
+    <div className='flex h-screen w-full'>
+      <div className='w-2/3 bg-red-400'>
+        <img src="" alt="" />
+        <img src="" alt="" />
+      </div>
+      <div className='w-1/3 bg-blue-300'>
+
+      </div>
+    </div>
   )
 }
 
